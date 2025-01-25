f6eeb08fb8d790ef67cf83d19ad219fcb5af7f8a
Ojas Srivastava <ojas1807@gmail.com>
2025-01-25 13:08:18 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Welcoe to gesLang,  a language for all.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
